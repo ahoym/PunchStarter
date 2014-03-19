3ec2113f6df705168f667d7ab3686ff55feac6da
Malcolm Ahoy <colmahoy@gmail.com>
2014-03-19 09:56:44 -0700

    Initial commit, change database to postgresql

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'turbolinks'
+
+gem 'bcrypt-ruby', '~> 3.1.2'
+
+group :doc do  
+  gem 'sdoc', require: false
+end
+
+group :development do
+  gem 'debugger'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'annotate'
+end
+
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'ejs'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PunchStarter::Application.config.secret_key_base = '461093ce05a99e58d25d9b15abb00c643afab4e035205911693decf1ed3bc440528d577c467c0b5ef2b563cc631aa28ec2e968a0f324a921331bf962da6060a7'
