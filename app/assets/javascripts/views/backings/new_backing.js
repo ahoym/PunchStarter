2562b3c63265ed248b691fc9a0e5f1c1e8986d9f
Malcolm Ahoy <colmahoy@gmail.com>
2014-03-28 16:50:27 -0700

    pre heroku push

--- app/assets/javascripts/views/backings/new_backing.js
+++ app/assets/javascripts/views/backings/new_backing.js
@@ -21,11 +21,11 @@ window.PunchStarter.Views.NewBacking = Backbone.View.extend ({
 		
 		project.backings().create({ investment: $investment, backed_project_id: this.model.id }, {
 			success: function () {
-				if (project.percentFunded() >= 100) {
-					window._successfullyFunded.add(project);
-				} else if (project.percentFunded() < 0) {
-					window._successfullyDefunded.add(project);
-				}
+				// if (project.percentFunded() >= 100) {
+				// 	window._successfullyFunded.add(project);
+				// } else if (project.percentFunded() < 0) {
+				// 	window._successfullyDefunded.add(project);
+				// }
 				 
 				Backbone.history.navigate("#/projects/" + project.id + "/" + project.collection.category.escape('name'),
 					{trigger: true} 
