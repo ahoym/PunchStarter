a8c3c596636f6aa2039fdf3a95d219b4a77748e8
Malcolm Ahoy <colmahoy@gmail.com>
2014-03-28 10:30:05 -0700

    set up for heroku again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,28 +1,15 @@
 PunchStarter::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
   config.cache_classes = true
 
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
-  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
   # Generate digests for assets URLs.
